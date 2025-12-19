c230420c2e28863e1b218ff2d8d3f77d89010a8a
adonismercado <adonis_mercado@ucne.edu.do>
2025-12-19 13:47:30 -0400

    feat ✨: Agrega modelo detalle EntradaDetalle

---
+++ GestorEntradas/Models/EntradaDetalle.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestorEntradas.Models;
+
+public class EntradaDetalle
+{
+    [Key]
+    public int EntradaDetalleId { get; set; }
+
+    public int EntradaId { get; set; }
+    public int ProductoId { get; set; }
+    public int Cantidad { get; set; }
+    public decimal Costo { get; set; } 
+}
