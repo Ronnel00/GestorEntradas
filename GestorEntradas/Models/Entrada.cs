89a8d9a71fdd3b2dc52ded6e116ef70e6e7909f9
adonismercado <adonis_mercado@ucne.edu.do>
2025-12-19 13:44:33 -0400

    feat ✨: Agrega modelo master Entrada

---
+++ GestorEntradas/Models/Entrada.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestorEntradas.Models;
+
+public class Entrada
+{
+    [Key]
+    public int EntradaId { get; set; }
+
+    [Required(ErrorMessage = "La fecha de entrada es obligatoria.")]
+    public DateTime FechaEntrada { get; set; }
+
+    [Required(ErrorMessage = "El concepto es obligatorio.")]
+    public string Concepto { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El total es obligatorio.")]
+    public decimal Total { get; set; }
+}
