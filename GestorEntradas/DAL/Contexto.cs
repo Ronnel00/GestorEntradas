811cf54b2f77702ab6bbf71a6902119dd93ddd24
adonismercado <adonis_mercado@ucne.edu.do>
2025-12-19 13:59:57 -0400

    feat ✨: Agrega el contexto para crear las tablas en la base de datos

---
+++ GestorEntradas/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestorEntradas.Models;
+
+namespace GestorEntradas.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Entrada> Entradas { get; set; }
+    public DbSet<EntradaDetalle> EntradaDetalles { get; set; }
+    public DbSet<Producto> Productos { get; set; }
+}
