b3a4c9e37b254ecf3d9ef8ea0f27ba416d0e0431
adonismercado <adonis_mercado@ucne.edu.do>
2025-12-19 18:58:39 -0400

    fix 🐛: Elimina metodo ListarProductos en EntradasService
    Ya el metodo existe en el servicio ProductosService

--- GestorEntradas/Services/EntradasService.cs
+++ GestorEntradas/Services/EntradasService.cs
@@ -117,14 +117,6 @@ public class EntradasService(IDbContextFactory<Contexto> DbFactory)
 
         return await contexto.SaveChangesAsync() > 0;
     }
-
-    public async Task<List<Producto>> ListarProductos()
-    {
-        await using var contexto = await DbFactory.CreateDbContextAsync();
-        return await contexto.Productos
-            .AsNoTracking()
-            .ToListAsync();
-    }
 }
 
 public enum TipoOperacion
